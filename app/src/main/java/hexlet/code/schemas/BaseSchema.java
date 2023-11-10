8ae4586e109821c32f0cc8692990ff57dc7cd993
suhoi <sushilyaz2000@gmail.com>
2023-11-10 22:11:15 +0300

    add desc BaseSchema

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,6 +1,11 @@
 package hexlet.code.schemas;
 
 public class BaseSchema {
+    /**
+     * Класс предназначен для расширения и метод ниже оверрайдед, ебаный чекстайл.
+     * @param validationElement
+     * @return boolean type
+     */
     public boolean isValid(Object validationElement) {
         return false;
     }
